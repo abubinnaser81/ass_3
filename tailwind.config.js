1857aeb3a6e9aa923e2e7d0da09694e818caa66d
abubinnaser81 <abunaser50032@gmail.com>
2025-02-01 00:23:25 +0600

    final amination

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        animation: {
+          'slide-in': 'slide-in 0.5s ease-out forwards',
+        },
+        keyframes: {
+          'slide-in': {
+            '0%': { transform: 'translateX(-100%)' },
+            '100%': { transform: 'translateX(0)' },
+          },
+        },
+      },
     },
     plugins: [],
   }
