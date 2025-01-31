6caf982e80c31a06d7c47981820db78d28dc79a6
abubinnaser81 <abunaser50032@gmail.com>
2025-01-31 15:19:20 +0600

    card section

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ['./src/**/*.{html,js}'],
+//   theme: {
+//     extend: {
+//       colors: {
+//         primary: "var(--primary)",
+//         secondary: "var(--secondary)",
+//         accent: "var(--accent)",
+//       },
+//     },
+//   },
+//   plugins: [],
+// }
